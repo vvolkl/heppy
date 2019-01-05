4a90d5c4f4f37c748fcaa0d451a352167182b7c0
Valentin Volkl <valentin.volkl@cern.ch>
2019-01-05 10:01:37 +0000

    fix typo

--- setup.py
+++ setup.py
@@ -103,9 +103,10 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
-    data_files = [("doc", glob.glob("doc/*"))],
+    data_files = [("doc", glob.glob("doc/*")),],
+    scripts = glob.glob("bin/*"),
 
-    scripts = glob.glob("bin/*.py")
+
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
