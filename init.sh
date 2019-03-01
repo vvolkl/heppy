122704d7facdcc2ef28c04d2097b5163b1954223
Valentin Volkl <valentin.volkl@cern.ch>
2019-03-01 22:29:51 +0100

    rename exe-scripts: chmod in init no longer needed

--- init.sh
+++ init.sh
@@ -3,10 +3,5 @@ export HEPPY=$PWD
 export PATH=$HEPPY/bin:$PATH
 export PYTHONPATH=$PWD/..:$PYTHONPATH
 
-# set up executable directory
-# cp scripts/*.py bin/
-# cp scripts/heppy bin/
-chmod +x bin/*.py 
-chmod +x bin/heppy
 
 
